1a90740560c4e24e327a10973e9f06330f8fdf9f
tonyadjei <tonyteyeadjei@gmail.com>
2021-02-05 21:23:11 +0100

    Added practice files for json-server to simulate API fetching with fetch()

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,37 @@
 <template>
-  <h1>Job Details page</h1>
-  <p>The job id is {{ id }}</p>
+<!-- VERY VERY VERY VERY IMORTANT!!!!!!!!!!!!!!!!!:  when you are getting data like data in json format from an API, and using the data returned in your template of your component -->
+<!-- please, please, please, always remember that, the browser will try to render what is in the template, and because the call to the API may take a few milliseconds to complete, -->
+<!-- it is very likely that the browser will render the content of the template, which at that time, has no value becuase it has not yet been set to the value we are expecting from the API -->
+<!-- it is therefore necessary, to make sure that, you don't access this data inside your template until it has been set to the value from the API call -->
+<!-- so you can use a v-if to make sure that, the data has been set, before displaying its content inside the template -->
+<!-- you can use the data itself as the condition for the v-if, null is a falsy value, so if we by default set the value of the data to null, v-if with a null value will evaluate to false, and hence the content will not show, when it is finally set to the value from the API, then it shows! -->
+<!-- NOTE: AN EMPTY ARRAY [] IS NOT A FALSY VALUE -->
+
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
+  
 </template>
 
 <script>
 export default {
-    props: ['id']
-    // data(){
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // }
+    props: ['id'],
+    data(){
+      return{
+        job: null
+      }
+    },
+    mounted(){
+      fetch('http://localhost:3000/jobs/' + this.id)
+      .then(response => response.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+    }
 }
 
 </script>
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,22 +1,30 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
+  <div v-if="jobs.length">
+      <div v-for="job in jobs" :key="job.id" class="job">
       <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
         <h2>{{ job.title }}</h2>
       </router-link> <!-- VERY IMPORTANT!!!!! <router-link> gets resolved to anchor tags(<a></a>) in the browser, so if you want to style your <router-link>, style <a></a> tags rather -->
+      </div>
+  </div>
+  <div v-else>
+      <p>Loading jobs...</p>
   </div>
 </template>
 
 <script>
 export default {
+    // this is inside Jobs.vue
     data(){
         return {
-            jobs: [
-                {title: "UI designer", id: 1, detail: "lorem"},
-                {title: "Web developer", id: 2, detail: "lorem"},
-                {title: "Graphic designer", id: 3, detail: "lorem"}
-            ]
+            jobs: []
         }
+    },
+    mounted(){ // we are using the mounted life cycle hook here, so that immediately the component has been mounted in the DOM, of course via the root componenet(in our case App.vue), then we fetch() the json data, set it equal to our jobs array variable, then cycle through it and display it via our template tag, we use a jobs array variable because we are expecting an array from the API
+        fetch('http://localhost:3000/jobs') // the fetch() API is a built-in JS method that allows us to make HTTP requests to servers or API's, it takes an endpoint as an argument. It is asynchronous and returns a promise
+        .then( response => response.json()) // inside the json file that is sent back to us, if the value of that json is an object, json() will parse that value into a JS object, if the json value we got back was an array, json() will parse it into a JS array, so json() does not only parse objects, it also parses arrays, basically any data type I guess
+        .then( data => this.jobs = data)
+        .catch(err => console.log(err.message))// remember, the .catch() method is used to catch any errors associated with the asynchronous funtions, it takes in a callback function which is fired when an error occurs, and it passes the error object as an argument to the callback function, this error object has a message property which tells the kind of error
     }
 }
 </script>
